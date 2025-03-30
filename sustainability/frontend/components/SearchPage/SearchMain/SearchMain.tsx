f88677b2c9cf015f3343475699fa1006f3bcdf87
Limbani <lchaponda@umass.edu>
2025-03-30 11:29:10 -0400

    added wrapper component for search bar

---
+++ sustainability/frontend/components/SearchPage/SearchMain/SearchMain.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchBar from "../SearchBar/SearchBar.tsx";
+
+const SearchMain = () => {
+  return (
+    <div>
+        <SearchBar/>
+    </div>
+  )
+}
+
+export default SearchMain;
