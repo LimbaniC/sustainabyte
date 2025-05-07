388f7e1d0dbd0546ff0bb412561bcc1f705ea485
nasMelody <nasrahassan759@gmail.com>
2025-05-07 16:59:49 -0400

    fixes

--- sustainability/frontend/components/DonationForm/DonationForm.tsx
+++ sustainability/frontend/components/DonationForm/DonationForm.tsx
@@ -42,7 +42,7 @@ const DonationForm = () => {
     };
 
     try {
-      const response = await fetch('http://localhost:5001/upload', {
+      const response = await fetch('http://localhost:5004/upload', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(foodData)
